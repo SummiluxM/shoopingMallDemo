11ee8b1632d96049989ca6a4a80071049fb5b11b
leihao <leihao@gmail.com>
2021-08-12 15:07:17 +0800

    create project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Button } from 'antd'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>HOLA</h1>
+        <>
+          <Button type="primary">Primary Button</Button>
+          <Button>Default Button</Button>
+          <Button type="dashed">Dashed Button</Button>
+          <br />
+          <Button type="text">Text Button</Button>
+          <Button type="link">Link Button</Button>
+        </>,
+      </div>
+    )
+  }
+}
+
